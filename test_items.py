f21507ed9a8fc1bde8179e92f4fb9172c255c9bd
Nikita <88321293+Marik758@users.noreply.github.com>
2023-08-21 15:42:08 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -11,5 +11,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_cart_button(browser):
     browser.get(link)
     assert browser.find_elements(By.XPATH, "//*[@id='add_to_basket_form']/button"), 'Button not found'
-    time.sleep(3)
+    time.sleep(30)
     browser.quit()
